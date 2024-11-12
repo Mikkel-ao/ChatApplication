5d63c7fd54487612464d01382e0eeae34bd3c617
mikkel.ao <mikkel.ao.dev@gmail.com>
2024-11-12 13:45:00 +0100

    Debugged client classes and ChatServer class

--- src/main/java/com/mikkel/chatapp/client/ClientHandler.java
+++ src/main/java/com/mikkel/chatapp/client/ClientHandler.java
@@ -1,4 +1,41 @@
 package com.mikkel.chatapp.client;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
-public class ClientHandler {
+public class ClientHandler implements Runnable {
+    private Socket clientSocket;
+    private List<ClientHandler> clients;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public ClientHandler(Socket clientSocket, List<ClientHandler> clients) throws IOException {
+        this.clientSocket = clientSocket;
+        this.clients = clients;
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public void run() {
+        try {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                // Broadcast message to all clients
+                for (ClientHandler aClient : clients) {
+                    aClient.out.println(inputLine);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                in.close();
+                out.close();
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- src/main/java/com/mikkel/chatapp/server/ChatServer.java
+++ src/main/java/com/mikkel/chatapp/server/ChatServer.java
@@ -1,10 +1,13 @@
 package com.mikkel.chatapp.server;
+import com.mikkel.chatapp.client.ClientHandler;
+
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ChatServer {
-    private static List<ClientHanbdler> clients = new ArrayList<>();
+    private static List<ClientHandler> clients = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(2000); // Port number 2000
@@ -16,11 +19,8 @@ public class ChatServer {
             // Spawn new thread for client
             ClientHandler clientThread = new ClientHandler(clientSocket, clients);
             clients.add(clientThread);
-            new Thread(ClientThread).start();
+            new Thread(clientThread).start();
         }
-
-        Socket clientSocket = serverSocket.accept();
-        System.out.println("Client connected");
     }
 }
 
